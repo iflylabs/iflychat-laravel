db91cdca40f521285dcd507969fb75c2075c8011
decentname <mr.hemangpta@gmail.com>
2017-05-12 15:45:41 +0530

    first commit

---
+++ src/iflychatServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace iflylabs\iflychat_laravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class iflychatServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //Register Controller
+        $this->app->make('iflylabs\iflychat_laravel\Http\Controllers\iflychatController');
+    }
+}
